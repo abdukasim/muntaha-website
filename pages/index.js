f32d40e9795335288149ad388957c8e9658671f1
abdukasim <abdukasimr@gmail.com>
2022-05-26 00:34:36 +0300

    activity section with assets and import into index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import AboutSection from "../components/aboutSection";
+import ActivitySection from "../components/activitySection";
 import Header from "../components/header";
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
 
       <Header />
       <AboutSection />
+      <ActivitySection />
     </div>
   );
 }
