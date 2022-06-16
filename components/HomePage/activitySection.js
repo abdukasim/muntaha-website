9a2189cfc9d5e3ad4309de43ca6ac29bd058f6e8
abdukasim <abdukasimr@gmail.com>
2022-06-16 15:19:28 +0300

    moved component into home page component folder

---
+++ components/HomePage/activitySection.js
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function ActivitySection() {
+  return (
+    <section className="activity-section">
+      <div className="flex flex-col xl:flex-row-reverse">
+        <div className="w-full mt-28 mb-10 pt-10 px-10  border-t-8 lg:border-t-16 border-l-8 lg:border-l-16 border-t-purple-500 border-l-purple-500">
+          <Image
+            width={582}
+            height={778}
+            layout="responsive"
+            src="/images/activityImg.png"
+            alt="Washing hands"
+          />
+        </div>
+        <div className="px-10 py-24 lg:pt-72 md:pl-20 xl:pl-44  md:bg-inherit flex items-start flex-col w-full">
+          <h2 className="font-black text-3xl text-purple-500 uppercase mb-6">
+            what we do
+          </h2>
+          <p className="mb-6 pb-3 font-light text-base md:text-2xl md:about-p-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Libero
+            ipsum quis aliquet semper ultricies. Feugiat justo at fermentum et
+            proin. At tellus elementum eget scelerisque. Sodales sit facilisi
+            senectus vitae faucibus magna. Neque adipiscing amet eget tellus.
+            Vitae eget morbi tellus, sit. Blandit vel tortor facilisis quam
+            turpis condimentum aliquam. Donec molestie suspendisse erat et.
+            Morbi habitant a massa arcu, varius facilisis egestas. Facilisis
+            sollicitudin neque amet placerat et congue suspendisse. Eget sed
+            arcu ac amet auctor at maecenas. Egestas ut purus, dui quis ut. Sed
+            et aenean amet malesuada
+          </p>
+          <Link href="/activity">
+            <a className="uppercase text-purple-400 font-bold text-xl md:text-3xl bg-transparent border border-solid border-purple-400 rounded-md drop-shadow-lg py-4 md:py-6 px-12 md:px-14 hover:bg-purple-400 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 ">
+              learn more
+            </a>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ components/HomePage/statSection.js
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import React from "react";
+
+export default function StatSection() {
+  return (
+    <section className="stat-section flex items-center justify-center my-40">
+      <div className="flex flex-col stat-pattern outline-16 md:w-max md:items-center md:justify-center outline-sky-blue outline-offset-4 outline m-3">
+        <h3 className="my-70px px-4 text-white font-normal text-lg sm:text-xl text-center uppercase">
+          at muntaha foundation we have provided
+        </h3>
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex flex-col mb-70px lg:ml-32">
+            <div className="mb-9 text-center">
+              <Image
+                width={116.67}
+                height={81.51}
+                src="/images/meal-icon.svg"
+                alt="Meal icon"
+              />
+            </div>
+            <span className="text-white font-normal text-xl sm:text-2xl text-center">
+              36,000 Meals
+            </span>
+          </div>
+          <div className="flex flex-col mb-70px md:mx-40">
+            <div className="mb-9 text-center">
+              <Image
+                width={116.67}
+                height={81.51}
+                src="/images/family-icon.svg"
+                alt="Meal icon"
+              />
+            </div>
+            <span className="text-white font-normal text-xl sm:text-2xl text-center">
+              For 56 Families
+            </span>
+          </div>
+          <div className="flex flex-col mb-70px lg:mr-32">
+            <div className="mb-9 text-center">
+              <Image
+                width={116.67}
+                height={81.51}
+                src="/images/woman-icon.svg"
+                alt="Meal icon"
+              />
+            </div>
+            <span className="text-white font-normal text-xl sm:text-2xl text-center">
+              Education
+              <br />
+              For 35 Young Women
+            </span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
