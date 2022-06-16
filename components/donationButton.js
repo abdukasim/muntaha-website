cb571e56b03a665ab97aa0b76cc0dce406ab5139
abdukasim <abdukasimr@gmail.com>
2022-06-16 15:16:18 +0300

    renamed or moved to home page folder in components

--- components/donationButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function DonationButton({ text }) {
-  return (
-    <button className="py-4 md:py-7 sm:px-7 md:px-8 bg-gray-200 rounded-md">
-      <p className="text-center text-xs sm:text-base">{text}</p>
-    </button>
-  );
-}
