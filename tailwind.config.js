027858db501684f6636c950257cf66120e932ffb
abdukasim <abdukasimr@gmail.com>
2022-06-21 15:41:16 +0300

    added breakpoints and theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
       colors: {
         "sky-blue": "#65A5E1",
         "purple-light": "#D065E1",
+        "purple-dark": "#4A017D",
+        "purple-text": " #2E005F",
         "brand-gray": "#F7F4FA",
         "brand-light": "#FFFAFA",
         "brand-dark": "#191A1C",
@@ -20,9 +22,16 @@ module.exports = {
       },
       spacing: {
         "70px": "70px",
+        "78px": "78px",
         "84px": "84px",
+        "88px": "88px",
         "90px": "90px",
+        "113px": "113px",
+        "120px": "120px",
+        "140px": "140px",
         "202px": "202px",
+        "282px": "282px",
+        "544px": "544px",
         "834px": "834px",
       },
       fontSize: {
