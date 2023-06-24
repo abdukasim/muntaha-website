70fb47caffd9f2373b9438a84635a4aab1899655
abdukasim <abdukasimr@gmail.com>
2023-06-24 09:25:07 +0300

    update wordpress link

--- utils/wordpress.js
+++ utils/wordpress.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://mywordpress.muntahafoundation.org/wp-json/wp/v2";
+const BASE_URL = "http://wordpress.muntahafoundation.org/wp-json/wp/v2";
 
 export async function getPosts() {
   const postsRes = await fetch(BASE_URL + "/posts?_embed&acf_format=standard");
