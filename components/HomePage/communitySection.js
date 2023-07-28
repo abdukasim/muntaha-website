b3c5bc4335c2b14f219740a65b27c68c160bf83d
abdukasim <abdukasimr@gmail.com>
2023-07-28 17:51:17 +0300

    font-size and spacing adjustment in community section

--- components/HomePage/communitySection.js
+++ components/HomePage/communitySection.js
@@ -2,37 +2,34 @@ import React from "react";
 
 export default function CommunitySection() {
   return (
-    <section className="mx-8 mb-34 flex flex-col items-center  justify-center gap-12 md:mx-28 lg:flex-row lg:gap-32">
+    <section className="mx-8 mb-16 flex flex-col items-center justify-center  gap-12 md:flex-row lg:mx-28 lg:mb-34 lg:gap-32">
       <div className="md:basis-2/5">
-        <h3 className="mb-4 text-4xl font-bold text-brand-blue md:mb-12 lg:text-6xl xl:text-7xl">
+        <h3 className="mb-4 text-5xl font-bold text-brand-blue md:mb-8 lg:text-6xl">
           Join our Community
         </h3>
-        <p className="text-lg text-brand-blue">
+        <p className="text-base text-brand-blue">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
           veritatis at placeat tempora expedita provident. Hic perferendis
-          mollitia harum aut eveniet explicabo vel, libero sequi quasi quo.
-          Quisquam, autem neque!
         </p>
       </div>
 
       <div className="w-full md:basis-1/2">
-        {/* create two input fields for full name and email address  */}
         <form className="flex flex-col space-y-4">
           <input
             type="text"
             name="full-name"
             id="full-name"
             placeholder="Full Name"
-            className="h-8 rounded-full pl-16 shadow-md md:h-24"
+            className="h-12 rounded-full pl-16 shadow-md md:h-16"
           />
           <input
             type="email"
             name="email"
             id="email"
             placeholder="Email Address"
-            className="h-8 rounded-full pl-16 shadow-md md:h-24"
+            className="h-12 rounded-full pl-16 shadow-md md:h-16"
           />
-          <button className="w-1/2 rounded-full bg-brand-blue py-5 text-brand-light">
+          <button className="w-1/2 rounded-full bg-brand-blue py-3 text-brand-light lg:py-5">
             Subscribe
           </button>
         </form>
