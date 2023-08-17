6ae3fc6b4b64ee328fb62694d7262fd38ca61a88
Biruk Ephrem <birukeph@gmail.com>
2023-08-18 02:06:40 +0300

    feat: works ui for big screen added

---
+++ components/svgComponents/economic_empowerment.js
@@ -0,0 +1,14 @@
+export default function EconomicEmpowerment() {
+  return (
+    <svg className="fill-brand-blue md:fill-white" id="Group_110" data-name="Group 110" width="85%" height="85%" viewBox="0 0 110 110">
+      <defs>
+        <clipPath id="clip-path">
+          <rect id="Rectangle_134" data-name="Rectangle 134" width="100%" height="100%" />
+        </clipPath>
+      </defs>
+      <g id="Group_109" data-name="Group 109" clipPath="url(#clip-path)">
+        <path id="Path_42" data-name="Path 42" d="M68.131,106.013a13.693,13.693,0,0,1-19.346-.027L7.054,64.254a26.006,26.006,0,0,1-6.889-16.5L0,13.7A13.663,13.663,0,0,1,13.7,0L47.768.165a26.041,26.041,0,0,1,16.5,6.875L106,48.771a13.72,13.72,0,0,1,.021,19.353L68.138,106.006h-.007ZM34.375,48.125a13.75,13.75,0,1,0-13.75-13.75,13.746,13.746,0,0,0,13.75,13.75" transform="translate(0 0)"fillRule="evenodd" />
+      </g>
+    </svg>
+  )
+}
---
+++ components/svgComponents/vocational_training.js
@@ -0,0 +1,17 @@
+export default function VocationalTraining() {
+  return (
+    <svg className="fill-brand-blue md:fill-white" width="85%" height="85%" viewBox="0 0 80 80">
+  <defs>
+    <clipPath id="clip-path">
+      <rect id="Rectangle_135" data-name="Rectangle 135" width="100%" height="100%" transform="translate(-0.33 0.076)"/>
+    </clipPath>
+  </defs>
+  <g id="Group_112" data-name="Group 112" transform="translate(0.33 -0.076)">
+    <g id="Group_111" data-name="Group 111" transform="translate(0 0)" clipPath="url(#clip-path)">
+      <path id="Path_43" data-name="Path 43" d="M69.828,79.8H9.975A9.973,9.973,0,0,1,0,69.828V54.865a9.973,9.973,0,0,1,9.975-9.975H69.828A9.973,9.973,0,0,1,79.8,54.865V69.828A9.973,9.973,0,0,1,69.828,79.8m0-44.889H9.975A9.973,9.973,0,0,1,0,24.938V9.975A9.973,9.973,0,0,1,9.975,0H69.828A9.973,9.973,0,0,1,79.8,9.975V24.938a9.973,9.973,0,0,1-9.975,9.975" transform="translate(0 -0.001)" fillRule="evenodd"/>
+    </g>
+  </g>
+</svg>
+
+  )
+}
