e3ba45858a5402e108cfd5d261832a479c763bfc
abdukasim <abdukasimr@gmail.com>
2023-08-31 12:00:12 +0300

    feat: trunc utility function added
    used for limiting number of character visible

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,10 @@
 export function getDate(date) {
-    return new Date(date).toLocaleDateString('en-GB', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    });
-  }
+  return new Date(date).toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
+
+export const trunc = (text, limit) =>
+  text.length > limit ? `${text.substring(0, limit)}...` : text;
